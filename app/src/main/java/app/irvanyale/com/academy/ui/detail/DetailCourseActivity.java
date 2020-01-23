beecf6db10cc31bc0cc00776ede382333f31d9f8
irvan <irvan@majoo.id>
2020-01-23 22:21:56 +0700

    init layout + assets

--- app/src/main/java/app/irvanyale/com/academy/ui/detail/DetailCourseActivity.java
+++ app/src/main/java/app/irvanyale/com/academy/ui/detail/DetailCourseActivity.java
@@ -1,18 +1,40 @@
 package app.irvanyale.com.academy.ui.detail;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
 
 import app.irvanyale.com.academy.R;
+import app.irvanyale.com.academy.data.CourseEntity;
+import app.irvanyale.com.academy.data.ModuleEntity;
+import app.irvanyale.com.academy.ui.reader.CourseReaderActivity;
+import app.irvanyale.com.academy.utils.DataDummy;
 
 public class DetailCourseActivity extends AppCompatActivity {
+
+    public static final String EXTRA_COURSE = "extra_course";
+    private Button btnStart;
+    private TextView textTitle;
+    private TextView textDesc;
+    private TextView textDate;
+    private ImageView imagePoster;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,16 +42,58 @@ public class DetailCourseActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail_course);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        btnStart = findViewById(R.id.btn_start);
+        textTitle = findViewById(R.id.text_title);
+        textDesc = findViewById(R.id.text_description);
+        textDate = findViewById(R.id.text_date);
+        RecyclerView rvModule = findViewById(R.id.rv_module);
+        imagePoster = findViewById(R.id.image_poster);
+
+        DetailCourseAdapter adapter = new DetailCourseAdapter();
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String courseId = extras.getString(EXTRA_COURSE);
+            if (courseId != null) {
+                List<ModuleEntity> modules = DataDummy.generateDummyModules(courseId);
+                adapter.setModules(modules);
+
+                for (int i = 0; i < DataDummy.generateDummyCourses().size(); i++) {
+                    CourseEntity courseEntity = DataDummy.generateDummyCourses().get(i);
+                    if (courseEntity.getCourseId().equals(courseId)) {
+                        populateCourse(courseEntity);
+                    }
+                }
             }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        rvModule.setNestedScrollingEnabled(false);
+        rvModule.setLayoutManager(new LinearLayoutManager(this));
+        rvModule.setHasFixedSize(true);
+        rvModule.setAdapter(adapter);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(rvModule.getContext(), DividerItemDecoration.VERTICAL);
+        rvModule.addItemDecoration(dividerItemDecoration);
     }
 
+    private void populateCourse(CourseEntity courseEntity) {
+        textTitle.setText(courseEntity.getTitle());
+        textDesc.setText(courseEntity.getDescription());
+        textDate.setText(getResources().getString(R.string.deadline_date, courseEntity.getDeadline()));
+
+        Glide.with(this)
+                .load(courseEntity.getImagePath())
+                .apply(RequestOptions.placeholderOf(R.drawable.ic_loading)
+                        .error(R.drawable.ic_error))
+                .into(imagePoster);
+
+        btnStart.setOnClickListener(v -> {
+            Intent intent = new Intent(DetailCourseActivity.this, CourseReaderActivity.class);
+            intent.putExtra(CourseReaderActivity.EXTRA_COURSE_ID, courseEntity.getCourseId());
+            startActivity(intent);
+        });
+    }
 }
--- app/src/main/java/app/irvanyale/com/academy/ui/home/HomeActivity.java
+++ app/src/main/java/app/irvanyale/com/academy/ui/home/HomeActivity.java
@@ -1,8 +1,11 @@
 package app.irvanyale.com.academy.ui.home;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
 
 import app.irvanyale.com.academy.R;
 
@@ -12,5 +15,13 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setElevation(0);
+        }
     }
 }
